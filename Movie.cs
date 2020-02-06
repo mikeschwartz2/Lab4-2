ee98a1932c09d7f603ba2ab49c5e8ec89bcf7907
Mike Schwartz <mikeschwartz@quickenloans.com>
2020-02-06 15:28:37 -0500

    First Commit

---
+++ Movie.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab4_2
+{
+    class Movie
+    {
+        private int catCount;
+        private string title;
+        private string category;
+        private int runTime;
+        private int yearReleased;
+
+        public int CatCount { get => catCount; }
+        public string Title { get => title; set => title = value; }
+        public string Category { get => category; set => category = value; }
+        public int RunTime { get => runTime; set => runTime = value; }
+        public int YearReleased { get => yearReleased; set => yearReleased = value; }
+
+        public Movie(int catCount, string title, string category, int runTime, int yearReleased)
+        {
+            this.catCount = catCount;
+            this.title = title;
+            this.category = category;
+            this.runTime = runTime;
+            this.yearReleased = yearReleased;
+        }
+
+        public override string ToString()
+        {
+            return $"=========================================================================\n" +
+                $"Movie: {title} " +
+                $"\nCategory: {category} \tRuntime: {runTime} minutes \tYear Released: {yearReleased}" +
+                $"\n=========================================================================\n";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Movie> movies = new List<Movie>();
+            string rerun = "";
+
+            movies.Add(new Movie(1, "Avengers", "action", 120, 2012));
+            movies.Add(new Movie(1, "John Wick", "action", 90, 2014));
+            movies.Add(new Movie(1, "Batman", "action", 130, 2010));
+            movies.Add(new Movie(2, "It Follows", "horror", 80, 2014));
+            movies.Add(new Movie(2, "The Babadook", "horror", 100, 2014));
+            movies.Add(new Movie(2, "Human Centepede", "horror", 110, 2009));
+            movies.Add(new Movie(3, "Deadpool", "comedy", 160, 2016));
+            movies.Add(new Movie(3, "Anchorman", "comedy", 115, 2004));
+            movies.Add(new Movie(3, "Dodgeball", "comedy", 95, 2001));
+            movies.Add(new Movie(4, "Lego Movie", "animation", 120, 2012));
+            movies.Add(new Movie(4, "How to Train Your Dragon", "animation", 120, 2012));
+            movies.Add(new Movie(4, "Monsters Inc", "animation", 120, 2012));
+
+            do
+            {
+
+                DisplayCat(movies);
+
+                CatSearch(movies);
+
+                rerun = Rerun(rerun);
+            }
+            while (rerun == "y");
+        }
+
+        static string MenuCheck(string search)
+        {
+            if (search == "1")
+            {
+                search = "action";
+            }
+            else if (search == "2")
+            {
+                search = "horror";
+            }
+            else if (search == "3")
+            {
+                search = "comedy";
+            }
+            else if (search == "4")
+            {
+                search = "animation";
+            }
+            return search;
+        }
+
+        static void DisplayCat(List<Movie> movies)
+        {
+            int i = 0;
+            Console.WriteLine("=========================================");
+            Console.WriteLine("           Category List");
+            Console.WriteLine("=========================================");
+
+            foreach (Movie m in movies)
+            {
+                if (m.CatCount != i)
+                {
+                    Console.WriteLine($"{m.CatCount}: {m.Category}");
+                    i++;
+                }
+            }
+        }
+
+        static void CatSearch(List<Movie> movies)
+        {
+            bool catCheck = false;
+            while (!catCheck)
+            {
+                Console.Write("Please enter category to search for: ");
+                string search = Console.ReadLine().ToLower();
+
+                search = MenuCheck(search);
+
+                foreach (Movie m in movies)
+                {
+                    if (m.Category.Contains(search))
+                    {
+                        Console.WriteLine(m);
+                        catCheck = true;
+                    }
+                }
+                if (!catCheck)
+                {
+                    Console.WriteLine("This is not a vailid category!");
+                }
+            }
+        }
+
+        static string Rerun(string rerun)
+        {
+            Console.WriteLine("Would you like to search for another category? y/n");
+            rerun = Console.ReadLine();
+            rerun = rerun.ToLower();
+
+            while (rerun != "y" && rerun != "n")
+            {
+                Console.WriteLine("This is not a valid input. Would you like to search again: y/n");
+                rerun = Console.ReadLine();
+                rerun = rerun.ToLower();
+            }
+            return rerun;
+        }
+    }
+}
